c5c3c4969bb5e460ebb9b6ca4a0a55eff96ddf2a
jen <jenny.diep@icloud.com>
2022-11-28 10:54:59 -0500

    Update TestCurrencyConvertor.java

--- ExpensesManager-master/src/test/TestCurrencyConvertor.java
+++ ExpensesManager-master/src/test/TestCurrencyConvertor.java
@@ -3,36 +3,50 @@ import java.util.Currency;
 
 public class TestCurrencyConvertor {
     //BLACK BOX TESTING
+    //see report for more details on the coverage
     
-    procedure convertCurrency() {
-        //list of currencies 
-        List<Currency> currencies = new ArrayList<Currency>();
-        currencies.add(Currency.getInstance("USD"));
-        currencies.add(Currency.getInstance("CAD"));
-        currencies.add(Currency.getInstance("GBP"));
-        currencies.add(Currency.getInstance("EUR"));
-        currencies.add(Currency.getInstance("CHF"));
-        currencies.add(Currency.getInstance("INR"));
-        currencies.add(Currency.getInstance("AUD"));
-
-        List<Int> amounts = new ArrayList<Int>();
-        for (int i = 0; i = 10000; i++) {
-            amounts.add(i);
-        }
-
-        //test all possible combinations of currencies and amounts
-        for (Currency currency : currencies) {
-            for (Int amount : amounts) {
-                //test convertCurrency method
-                convertCurrency(currency, amount);
-            }
-        }
-
-        //in case of invalid currency or amount, throw exception
-        try {
-            convertCurrency(Currency.getInstance("ABC"), 100);
-        } catch (Exception e) {
-            //expected exception
-        }
+   @Test 
+   // test if amount entered is between 0 and 10000
+    public void testAmount() {
+         CurrencyConvertor convertor = new CurrencyConvertor();
+         double amount = 1000;
+         String from = "USD";
+         String to = "CAD";
+         CurrencyConversion conversion = new CurrencyConversion();
+         conversion.getRates().put("USD", 1.0);
+         conversion.getRates().put("CAD", 0.74);
+         double result = convertor.convert(amount, from, to, conversion);
+         assertEquals(740.0, result, 0.0);
+        //test if amount entered is less than 0
+         amount = -1;
+         result = convertor.convert(amount, from, to, conversion);
+         assertEquals(0.0, result, 0.0);
+        //test if amount entered is greater than 10000
+        amount = 20000;
+        result = convertor.convert(amount, from, to, conversion);
+        assertEquals(0.0, result, 0.0);
     }
-}
+
+    @Test
+    //test if currency entered is valid
+    public void testCurrency() {
+        CurrencyConvertor convertor = new CurrencyConvertor();
+        double amount = 1000;
+        String from = "USD";
+        String to = "CAD";
+        CurrencyConversion conversion = new CurrencyConversion();
+        conversion.getRates().put("USD", 1.0);
+        conversion.getRates().put("CAD", 0.74);
+        double result = convertor.convert(amount, from, to, conversion);
+        assertEquals(740.0, result, 0.0);
+        //test if currency entered is invalid
+        from = "KYD";
+        to = "CAD";
+        result = convertor.convert(amount, from, to, conversion);
+        assertEquals(0.0, result, 0.0);
+        from = "USD";
+        to = "KYD";
+        result = convertor.convert(amount, from, to, conversion);
+        assertEquals(0.0, result, 0.0);
+    }
+} 
